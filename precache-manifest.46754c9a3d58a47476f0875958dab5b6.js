400c0f4c612c0bfe88d12539ae32a1eecaf47770
betonblockchain <68390989+betonblockchain@users.noreply.github.com>
2020-08-16 02:02:39 +0200

    Add files via upload
    site

---
+++ precache-manifest.46754c9a3d58a47476f0875958dab5b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f2bfd47d97d16475fabcfcbe6e241f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddf1646da7a73d4e9d5e",
+    "url": "/static/css/main.96036274.chunk.css"
+  },
+  {
+    "revision": "5157c2459b11eff7eb29",
+    "url": "/static/js/2.eea06c48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eea06c48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf1646da7a73d4e9d5e",
+    "url": "/static/js/main.1d0bfdcf.chunk.js"
+  },
+  {
+    "revision": "e6ce9dae5c104b8b20b0",
+    "url": "/static/js/runtime-main.3fc3c0c7.js"
+  }
+]);
