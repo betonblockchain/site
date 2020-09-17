d18670d4efa8c45ce7ce5d33457ff5d6f2ef568b
betonblockchain <68390989+betonblockchain@users.noreply.github.com>
2020-09-17 18:04:50 +0200

    sdf

---
+++ static/js/precache-manifest.8d8f49cc38280174a397e4f1c4874694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2713b19af3dd876b0bd8ced0b675d86c",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "4246f97cba258e44a49d",
+    "url": "/site/static/css/main.0c1129da.chunk.css"
+  },
+  {
+    "revision": "d4773d49d719151f04b4",
+    "url": "/site/static/js/2.2b1671bc.chunk.js"
+  },
+  {
+    "revision": "e980a7f3cee8e03adfb625e54adfa237",
+    "url": "/site/static/js/2.2b1671bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4246f97cba258e44a49d",
+    "url": "/site/static/js/main.af7699cb.chunk.js"
+  },
+  {
+    "revision": "47375c2cddd564eee261",
+    "url": "/site/static/js/runtime-main.6a0a589b.js"
+  }
+]);
